4ada55c7212112269b09c7ad2cb0ae435a42eff2
Dmytro Holovskyi <80936904545dgold@gmail.com>
2024-05-01 08:34:21 +0300

    task-1
    task-1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+let lastWathedFilm = prompt('Один из последних просмотренных фильмов?', '');
+let whatTheSkore = prompt('На сколько оцените его?', '');
+let lastWathedFilmTwo = prompt('Один из последних просмотренных фильмов?', '');
+let whatTheSkoreWTho = prompt('На сколько оцените его?', '');
+
+personalMovieDB[lastWathedFilm] = whatTheSkore;
+personalMovieDB[lastWathedFilmTwo] = whatTheSkoreWTho;
+
+console.log(numberOfFilms);
+
+console.log(personalMovieDB);
