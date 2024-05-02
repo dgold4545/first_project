e5cc5735e61f6d89af09b870f6cc5a95d5d4c726
DGOLD7788 <80936904545dgold@gmail.com>
2024-05-02 09:20:36 +0300

    start second

--- js/learning_02.js
+++ js/learning_02.js
@@ -1,1 +1,25 @@
 'use strict';
+
+'use strict';
+
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+let lastWathedFilm = prompt('Один из последних просмотренных фильмов?', '');
+let whatTheSkore = prompt('На сколько оцените его?', '');
+let lastWathedFilmTwo = prompt('Один из последних просмотренных фильмов?', '');
+let whatTheSkoreWTho = prompt('На сколько оцените его?', '');
+
+personalMovieDB[lastWathedFilm] = whatTheSkore;
+personalMovieDB[lastWathedFilmTwo] = whatTheSkoreWTho;
+
+console.log(numberOfFilms);
+
+console.log(personalMovieDB);
