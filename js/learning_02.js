99a4d7be18391461d64be43d533c719146b89670
DGOLD7788 <80936904545dgold@gmail.com>
2024-05-02 13:01:50 +0300

    add task funk

--- js/learning_02.js
+++ js/learning_02.js
@@ -51,3 +51,46 @@ if (personalMovieDB.count < 10) {
   console.log(numberOfFilms);
 
 console.log(personalMovieDB);
+
+
+
+function sayHello(name) {
+  return `Привет, ${name}!`;
+}
+
+
+function returnNeighboringNumbers(number) {
+  return [number - 1, number, number + 1]
+}
+
+
+returnNeighboringNumbers(18);
+
+
+ function sayHello(userName) {
+   return ('Привет,' + userName + '!');
+ }
+console.log(sayHello('aynjh'));
+ 
+
+
+
+function getMathResult(num1,num2) {
+  const numberOne = num1;
+  const numberTwo = num2;
+
+  if (typeof numberTwo !== 'number' || numberTwo <= 0) {
+    return numberOne;
+  }
+
+  let result = [];
+
+  for (let i = 0; i < numberTwo; i += 1) {
+    result.push(numberOne * (i + 1));
+  }
+
+  return result.join('---');
+  
+}
+
+console.log(getMathResult(10, 5));
