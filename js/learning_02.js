390ebf2c32383af13a2a333c1ea729a840c6a61c
DGOLD7788 <80936904545dgold@gmail.com>
2024-05-02 10:20:03 +0300

    tassk_1

--- js/learning_02.js
+++ js/learning_02.js
@@ -1,8 +1,10 @@
 'use strict';
 
-'use strict';
+let numberOfFilms;
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+while (!numberOfFilms || numberOfFilms.length > 50) {
+  numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+}
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -12,13 +14,26 @@ const personalMovieDB = {
   privat: false,
 };
 
-let lastWathedFilm = prompt('Один из последних просмотренных фильмов?', '');
-let whatTheSkore = prompt('На сколько оцените его?', '');
-let lastWathedFilmTwo = prompt('Один из последних просмотренных фильмов?', '');
-let whatTheSkoreWTho = prompt('На сколько оцените его?', '');
+// let lastWathedFilm = prompt('Один из последних просмотренных фильмов?', '');
+// let whatTheSkore = prompt('На сколько оцените его?', '');
+// let lastWathedFilmTwo = prompt('Один из последних просмотренных фильмов?', '');
+// let whatTheSkoreWTho = prompt('На сколько оцените его?', '');
 
-personalMovieDB[lastWathedFilm] = whatTheSkore;
-personalMovieDB[lastWathedFilmTwo] = whatTheSkoreWTho;
+// while (!lastWathedFilm || lastWathedFilm.length >= 50) {
+//   lastWathedFilm = prompt('Сколько фильмов вы уже посмотрели?', '');
+// }
+// while (!whatTheSkore || whatTheSkore.length >= 50) {
+//   whatTheSkore = prompt('Сколько фильмов вы уже посмотрели?', '');
+// }
+// while (!lastWathedFilmTwo || lastWathedFilmTwo.length >= 50) {
+//   lastWathedFilmTwo = prompt('Сколько фильмов вы уже посмотрели?', '');
+// }
+// while (!whatTheSkoreWTho || whatTheSkoreWTho.length >= 50) {
+//   whatTheSkoreWTho = prompt('Сколько фильмов вы уже посмотрели?', '');
+// }
+
+// personalMovieDB[lastWathedFilm] = whatTheSkore;
+// personalMovieDB[lastWathedFilmTwo] = whatTheSkoreWTho;
 
 console.log(numberOfFilms);
 
